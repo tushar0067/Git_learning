bf6a778e5ed8544b1c6caf01a696bad56a3d6788
tushar0067 <tushar.2226csit1003@kiet.edu>
2025-07-05 00:14:54 +0530

    Payment first commit

--- first.js
+++ first.js
@@ -3,3 +3,9 @@ console.log(button);
 
 const footer="added a footer";
 console.log(footer);
+const login ="Added a login"
+console.log(login);
+
+const payment="Added a payment module"
+console.log(payment);
+
